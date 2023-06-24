f5e1cdc93dc84b3fad312e653d9da9a7b1e55268
Vera Samohina <samohinavera44@gmail.com>
2023-06-24 19:59:13 +0300

    add new field for class Channel and func to_json and get_service

--- src/channel.py
+++ src/channel.py
@@ -14,7 +14,27 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id                                                   # id канала
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']                             # Название канала
+        self.description = channel['items'][0]['snippet']['description']                 # Описание канала
+        self.url = f'https://www.youtube.com/channel/{channel_id}'                       # ссылка на канал
+        self.subscriberCount = channel['items'][0]['statistics']['subscriberCount']      # количество подписчиков
+        self.video_count = channel['items'][0]['statistics']['videoCount']               # количество видео
+        self.viewCount = channel['items'][0]['statistics']['viewCount']                  # количество просмотров
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        return youtube
+
+    def to_json(self, filename):
+        data = self.__dict__
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
